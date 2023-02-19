77af9e68fd9ed1eb75bf409b3da68bce3f403f8c
white piano <startoflateness@gmail.com>
2023-02-19 14:22:25 +0900

    try: realize that  don't need to merge arrays

--- jlis/solution.cpp
+++ jlis/solution.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <algorithm>
 
-int C, N, M;
-int nArr[100], mArr[100];
-long long merged[201] = {INT64_MIN};
-int cache[201];
-int ans = 0;
+#define FOR(i, st, en) for (int i = st; i < en; i++)
 
-void merge(int nc, int mc);
-int calcLis(int idx);
+int C, N, M;
+long nArr[101] = {INT64_MIN},
+     mArr[101] = {INT64_MIN};
+int cache[101][101];
+
+int calcLis(int ni, int mi);
 void clearCache();
 
 int main()
@@ -17,61 +17,43 @@ int main()
     for (int c = 0; c < C; c++)
     {
         std::cin >> N >> M;
-        for (int n = 0; n < N; n++)
-            std::cin >> nArr[n];
-        for (int m = 0; m < M; m++)
-            std::cin >> mArr[m];
 
-        ans = 0;
+        FOR(n, 1, N + 1)
+        std::cin >> nArr[n];
+
+        FOR(m, 1, M + 1)
+        std::cin >> mArr[m];
+
         clearCache();
-
-        merge(0, 0);
-        std::cout << ans << std::endl;
+        std::cout << calcLis(0, 0) - 2 << std::endl;
     }
 
     return 0;
 }
 
-void merge(int nc, int mc)
+int calcLis(int ni, int mi)
 {
-    if (nc == N && mc == M)
-    {
-        calcLis(0);
-        ans = std::max(cache[0] - 1, ans);
-        clearCache();
-        return;
-    }
-
-    if (nc != N)
-    {
-        merged[nc + mc + 1] = nArr[nc];
-        merge(nc + 1, mc);
-    }
-
-    if (mc != M)
-    {
-        merged[nc + mc + 1] = mArr[mc];
-        merge(nc, mc + 1);
-    }
-}
-
-int calcLis(int idx)
-{
-    int &ret = cache[idx];
+    int &ret = cache[ni][mi];
     if (ret != -1)
         return ret;
 
-    ret = 1;
-    for (int nxt = idx + 1; nxt <= N + M; nxt++)
-    {
-        if (merged[nxt] > merged[idx])
-            ret = std::max(calcLis(nxt) + 1, ret);
-    }
+    ret = 2;
+    int cur = std::max(nArr[ni], mArr[mi]);
+
+    FOR(nxt, ni + 1, N + 1)
+    if (nArr[nxt] > cur)
+        ret = std::max(calcLis(nxt, mi) + 1, ret);
+
+    FOR(nxt, mi + 1, M + 1)
+    if (mArr[nxt] > cur)
+        ret = std::max(calcLis(ni, nxt) + 1, ret);
+
     return ret;
 }
 
 void clearCache()
 {
-    for (int i = 0; i < 201; i++)
-        cache[i] = -1;
+    FOR(i, 0, 101)
+    FOR(j, 0, 101)
+    cache[i][j] = -1;
 }
