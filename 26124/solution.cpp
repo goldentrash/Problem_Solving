e519465a328280130bf9dc875b690ef33a19e942
white piano <startoflateness@gmail.com>
2023-03-07 12:19:04 +0900

    timeout(2)

--- 26124/solution.cpp
+++ 26124/solution.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <utility>
-#include <cstring>
 #include <queue>
+#include <cstring>
 
 int H, W;
 int ans;
-std::pair<int, int> lowest;
 
 int map[1'000][1'000];
-bool checked[1'000][1'000];
-std::pair<int, int> dir[4] = {std::pair(-1, 0), std::pair(1, 0), std::pair(0, -1), std::pair(0, 1)};
+std::pair<int, int> dir[4] = {
+    std::pair(-1, 0), std::pair(1, 0), std::pair(0, -1), std::pair(0, 1)};
 
+bool isPossible();
 void calc();
-bool setup(std::pair<int, int> spot);
+void setup(std::pair<int, int> spot);
 
 int main()
 {
     std::cin >> H >> W;
 
-    lowest = std::pair(0, 0);
     for (int h = 0; h < H; h++)
         for (int w = 0; w < W; w++)
-        {
             std::cin >> map[h][w];
-            if (map[h][w] < map[lowest.first][lowest.second])
-                lowest = std::pair(h, w);
-        }
+
+    if (!isPossible())
+    {
+        std::cout << -1 << std::endl;
+        return 0;
+    }
 
     ans = 0;
     calc();
@@ -34,77 +35,92 @@ int main()
     return 0;
 }
 
+bool isPossible()
+{
+    for (int h = 0; h < H; h++)
+        for (int w = 0; w < W; w++)
+        {
+            if (map[h][w] == -1)
+                continue;
+
+            for (std::pair<int, int> d : dir)
+            {
+                std::pair next(h + d.first, w + d.second);
+
+                if (next.first < 0 || next.first == H || next.second < 0 || next.second == W ||
+                    map[next.first][next.second] == -1)
+                    continue;
+
+                if (std::abs(map[h][w] - map[next.first][next.second]) > 1)
+                    return false;
+            }
+        }
+
+    return true;
+}
+
 void calc()
 {
     // find unchecked brightest spot
-    std::pair<int, int> brightest = lowest;
+    std::pair<int, int> brightest = std::pair(0, 0);
     for (int h = 0; h < H; h++)
         for (int w = 0; w < W; w++)
-            if (map[brightest.first][brightest.second] < map[h][w] &&
-                !checked[h][w])
+            if (map[brightest.first][brightest.second] < map[h][w])
                 brightest = std::pair(h, w);
 
-    if (brightest == lowest || map[brightest.first][brightest.second] == 0) // if all lights already setted up
+    // if all lights already setted up
+    if (map[brightest.first][brightest.second] == 0)
         return;
 
     // setup light
-    if (setup(brightest))
-        calc(); // repeat if setup was successful
+    setup(brightest);
+    ans++;
+
+    // repeat until all lights setted up
+    calc();
 }
 
-bool setup(std::pair<int, int> spot)
+void setup(std::pair<int, int> spot)
 {
-    std::pair<int, int> del(-1, -1);
+    bool visit[H][W];
+    std::memset(visit, 0, H * W * sizeof(bool));
+
     int light = map[spot.first][spot.second];
     int cnt = 0;
 
-    bool visit[1'000][1'000];
-    std::memset(visit, 0, H * W * sizeof(bool));
-
     std::queue<std::pair<int, int>> q;
+    std::pair<int, int> del(-1, -1);
+
     q.push(spot);
     visit[spot.first][spot.second] = true;
+    map[spot.first][spot.second] = 0;
     q.push(del);
 
-    while (!q.empty())
+    while (q.size() != 1)
     {
         std::pair now = q.front();
         q.pop();
 
         if (now == del)
         {
-            if (q.empty())
-                break;
             q.push(del);
             cnt++;
-
-            if (cnt == light)
-                break;
-            else
-                continue;
+            continue;
         }
 
-        if (map[now.first][now.second] < light - cnt)
-        {
-            ans = -1;
-            return false;
-        }
-        else if (map[now.first][now.second] == light - cnt)
-            checked[now.first][now.second] = true;
+        if (map[now.first][now.second] == light - cnt)
+            map[now.first][now.second] = 0;
 
         for (std::pair<int, int> d : dir)
         {
             std::pair next(now.first + d.first, now.second + d.second);
 
             if (next.first < 0 || next.first == H || next.second < 0 || next.second == W ||
-                visit[next.first][next.second] || map[next.first][next.second] == -1)
+                map[next.first][next.second] < 1 || visit[next.first][next.second])
                 continue;
 
             q.push(next);
             visit[next.first][next.second] = true;
         }
     }
-
-    ans++;
-    return true;
 }
