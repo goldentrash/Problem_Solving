c335fe335554750eeec0965eb188ae5c3b54dd13
white piano <startoflateness@gmail.com>
2023-03-08 10:09:11 +0900

    solve: 부분합은 정수여야만 함!

--- c466/solution.cpp
+++ c466/solution.cpp
@@ -9,11 +9,24 @@ long long ans;
 int main()
 {
     std::cin >> N;
+
+    if (N < 3)
+    {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+
     pSum[0] = 0;
     for (int n = 0; n < N; n++)
     {
         std::cin >> arr[n];
         pSum[n + 1] = arr[n] + pSum[n];
+    }
+
+    if (pSum[N] % 3 != 0)
+    {
+        std::cout << 0 << std::endl;
+        return 0;
     }
 
     long long trg = pSum[N] / 3;
@@ -27,7 +40,7 @@ int main()
     }
 
     ans = 0;
-    for (int n = N - 1; n > 0; n--)
+    for (int n = N - 1; n > 1; n--)
     {
         if (pSum[N] - pSum[n] == trg)
             ans += cnt[n - 1];
