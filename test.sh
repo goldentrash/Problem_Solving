30a13ec83e2faf25196be1a1398f48887aa66851
white piano <startoflateness@gmail.com>
2023-04-12 17:07:30 +0900

    update: test 전에 반드시 build 되도록 vscode task에 추가

--- test.sh
+++ test.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-if [ ! -d $1 ]
-then
-  1>&2 echo "there's no problem. Are you sure it's $1?"
-  exit 2
-fi
-
 CNT=1
-while [ -f "./$1/input$CNT.txt" ]
+while [ -f "$1/input$CNT.txt" ]
 do
-  RET=`"./$1/solution" < "./$1/input$CNT.txt" | \
-    diff -b "./$1/output$CNT.txt" -`
+  RET=`"$1/solution" < "$1/input$CNT.txt" | \
+    diff -b "$1/output$CNT.txt" -`
 
   [ -z "$RET" ] && echo "clear case $CNT!" || echo "fail case $CNT\r\n$RET"
   echo
