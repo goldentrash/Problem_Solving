24f72cb11cacc5283c817ba4f5ce55471afcd9e6
white piano <startoflateness@gmail.com>
2023-04-12 17:16:07 +0900

    runtimeerr: 아마도 out of bounds

---
+++ c839/solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <list>
+
+int N;
+std::list<int> carr[100'000];
+bool visit[100'000];
+
+double dfs(int c);
+
+int main()
+{
+    std::cin >> N;
+
+    for (int n = 1; n < N; n++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        carr[a].push_back(b);
+        carr[b].push_back(a);
+    }
+
+    visit[1] = true;
+    std::cout << std::setprecision(10) << dfs(1) << std::endl;
+
+    return 0;
+}
+
+double dfs(int c)
+{
+    int cnt = 0;
+    double total = 0.0;
+    for (int nxt : carr[c])
+    {
+        if (visit[nxt])
+            continue;
+
+        visit[nxt] = true;
+        total += dfs(nxt) + 1;
+        visit[nxt] = false;
+        cnt++;
+    }
+    if (cnt)
+        return total / cnt;
+    else
+        return 0;
+}
