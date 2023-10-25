9775e739589ee4ccda1d9e67254fe57e026a7054
goldentrash <startoflateness@gmail.com>
2023-10-25 21:35:30 +0900

    solve: 종만북에서 완전 동일한 문제를 푼 적이 있다

--- b1285/solution.cpp
+++ b1285/solution.cpp
@@ -5,6 +5,8 @@ int T, N;
 int tastiness[100'000];
 long long pSum[100'001];
 
+long long calc(int st, int en);
+
 int main()
 {
     std::cin >> T;
@@ -18,17 +20,26 @@ int main()
             pSum[n + 1] = tastiness[n] + pSum[n];
         }
 
-        std::sort(tastiness, tastiness + N);
-
-        long long adel = pSum[N] - tastiness[0];
-        for (int n = 1; n < N; n++)
-        {
-            if (tastiness[n] < 0)
-                adel -= tastiness[n];
-        }
-
-        std::cout << ((pSum[N] > adel) ? "YES" : "NO") << std::endl;
+        std::cout << ((pSum[N] > calc(0, N - 1) && pSum[N] > calc(1, N)) ? "YES" : "NO") << std::endl;
     }
 
     return 0;
+}
+
+long long calc(int st, int en)
+{
+    if (en - st == 1)
+        return tastiness[st];
+
+    int half = (st + en) / 2;
+
+    long long lside = tastiness[half - 1];
+    for (int i = half - 2; i >= st; i--)
+        lside = std::max(lside, pSum[half] - pSum[i]);
+
+    long long rside = tastiness[half];
+    for (int i = half + 1; i < en; i++)
+        rside = std::max(rside, pSum[i + 1] - pSum[half]);
+
+    return std::max({lside + rside, calc(st, half), calc(half, en)});
 }
